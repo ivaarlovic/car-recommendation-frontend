d3485035a09fcab278493752a0d8847d427366bc
ivaarlovic <iva.arlovic03@gmail.com>
2026-06-01 18:38:41 +0200

    fix backend API url to render

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://car-recommendation-app-production.up.railway.app/api",
+  baseURL: "https://car-recommendation-app-backend.onrender.com/api",
 });
 
 export default api;
