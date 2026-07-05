9ce87f3271e16b24def9156aabd5e0869f9c07aa
ivaarlovic <iva.arlovic03@gmail.com>
2026-07-05 15:16:25 +0200

    poboljsanje verzije

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,7 @@ import LoginPage from "./pages/LoginPage";
 import SurveyCompletedPage from "./components/SurveyCompletePage";
 
 function App() {
-  const [acceptedGdpr, setAcceptedGdpr] = useState(
-    () => localStorage.getItem("gdprAccepted") === "true",
-  );
-
+  const [acceptedGdpr, setAcceptedGdpr] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   // Provjera da li je korisnik ulogiran
@@ -24,12 +21,35 @@ function App() {
       const twoWeeks = 14 * 24 * 60 * 60 * 1000;
       if (Date.now() - parseInt(loginTime) < twoWeeks) {
         setIsLoggedIn(true);
+      } else {
+        // Istekao login
+        localStorage.removeItem("surveyUser");
+        localStorage.removeItem("loginTime");
+        setIsLoggedIn(false);
       }
+    }
+
+    const gdprAccepted = localStorage.getItem("gdprAccepted");
+    const gdprTimestamp = localStorage.getItem("gdprTimestamp");
+
+    if (gdprAccepted && gdprTimestamp) {
+      const twoWeeks = 14 * 24 * 60 * 60 * 1000;
+      const isGdprValid = Date.now() - parseInt(gdprTimestamp) < twoWeeks;
+
+      if (isGdprValid) {
+        setAcceptedGdpr(true);
+      } else {
+        localStorage.removeItem("gdprAccepted");
+        localStorage.removeItem("gdprTimestamp");
+        setAcceptedGdpr(false);
+      }
+      setAcceptedGdpr(false);
     }
   }, []);
 
   const handleAcceptGdpr = () => {
     localStorage.setItem("gdprAccepted", "true");
+    localStorage.setItem("gdprTimestamp", Date.now().toString());
     setAcceptedGdpr(true);
   };
 
@@ -46,21 +66,21 @@ function App() {
             !isLoggedIn ? (
               <LoginPage setIsLoggedIn={setIsLoggedIn} />
             ) : (
-              <Navigate to="/home" />
+              <Navigate to="/home" replace />
             )
           }
         />
 
         <Route
           path="/home"
-          element={isLoggedIn ? <HomePage /> : <Navigate to="/login" />}
+          element={isLoggedIn ? <HomePage /> : <Navigate to="/login" replace />}
         />
 
         <Route path="/survey-completed" element={<SurveyCompletedPage />} />
 
         <Route
           path="*"
-          element={<Navigate to={isLoggedIn ? "/home" : "/login"} />}
+          element={<Navigate to={isLoggedIn ? "/home" : "/login"} replace />}
         />
       </Routes>
     </div>
